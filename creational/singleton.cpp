691ff1def26104a290ab069ff2c78e3acfd2734c
oshkola <olsh91@gmail.com>
2021-01-31 12:36:35 +0200

    singleton implementation

---
+++ creational/singleton.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Singleton
+{
+	Singleton() = default;
+
+public:
+	//public + delete provides clear error messages
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+	//lazy initialization of _instance
+	static Singleton& get_instance()
+	{
+  		//static + local variable does the trick  
+        static Singleton _instance;
+		return _instance;
+	}
+};
+
+
+int main()
+{
+
+	Singleton& s = Singleton::get_instance();
+
+}
